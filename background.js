200224e6df598696d7d5d7426f9a4ad69eb27a0b
alboxer2000 <179520006+alboxer2000@users.noreply.github.com>
2025-09-02 22:25:51 +0200

    Update background.js

--- background.js
+++ background.js
@@ -2,5 +2,6 @@ browser.runtime.onMessage.addListener((message) => {
   if (message.action === "updateBadge") {
     browser.browserAction.setBadgeText({ text: message.count.toString() });
     browser.browserAction.setBadgeBackgroundColor({ color: "#4688F1" });
+    browser.browserAction.setBadgeTextColor({ color: "#FFFFFF" });
   }
 });
